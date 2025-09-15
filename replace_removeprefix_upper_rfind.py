8d11491d7a34b704f669daa326699899533ee3c2
bob@example.com <bob@example.com>
2025-09-14 21:34:50 -0500

    .replace

--- replace_removeprefix_upper_rfind.py
+++ replace_removeprefix_upper_rfind.py
@@ -7,3 +7,6 @@
 print("Bienvenido, en el siguiente programa se mostraran ejemplos de como usar los metodos replace, removeprefix, upper y rfind \n")
 
 var=input("primero definimos una variable, puedes escribir una frase o plabra: ")
+
+print("ahora reemplazaremos las a y o por numeros")
+print(var.replace("a","4").replace("o","0"))
