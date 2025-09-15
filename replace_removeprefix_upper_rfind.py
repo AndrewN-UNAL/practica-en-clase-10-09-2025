5d64ee027c25d9246feaf859408acff4f47a8896
bob@example.com <bob@example.com>
2025-09-14 21:33:56 -0500

    initial variable

--- replace_removeprefix_upper_rfind.py
+++ replace_removeprefix_upper_rfind.py
@@ -1,7 +1,9 @@
 #autor: Andrés F. Núñez
 #fecha: 14/09/2025
 
-var="nombre del curso"
-print(var)
-print(var.replace("curso","perro"))
-print(var)
+
+#Ejercicios de variables y metodos
+
+print("Bienvenido, en el siguiente programa se mostraran ejemplos de como usar los metodos replace, removeprefix, upper y rfind \n")
+
+var=input("primero definimos una variable, puedes escribir una frase o plabra: ")
